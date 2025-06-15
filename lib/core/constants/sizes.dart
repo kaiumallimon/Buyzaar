0c8e803f3d1ed45e1d43bc069333c23b08ecd979
Kaium Al Limon <limon.3cs@gmail.com>
2025-06-15 09:38:29 +0600

    Add AppSizes class and extend AppColors with additional color constants

---
+++ lib/core/constants/sizes.dart
@@ -0,0 +1,4 @@
+class AppSizes {
+  static const double defaultSidePadding = 20;
+  static const double textFieldSidePadding = 15;
+}
--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,4 +1,8 @@
 class AppColors {
   static const int darkPrimary = 0xFF1A1A1A;
   static const int lightPrimary = 0xFFFFFFFF;
+  static const int lightGreyText = 0xFF808080;
+  static const int borderColor = 0xFFE6E6E6;
+  static const int buttonBorderColor = 0xFFCCCCCC;
+  static const int hintTextColor = 0xFF999999;
 }
